460bf9fdc95e3e23fe7f83840f8d2a472461bb05
Haochen Zhang <hz112@rice.edu>
2026-03-24 23:17:18 -0500

    0324

--- scripts/evaluate/fid_synthu.sh
+++ scripts/evaluate/fid_synthu.sh
@@ -20,6 +20,25 @@ export CUDA_VISIBLE_DEVICES=1
 #    --save_path "./fid_results/vae_embed_generation.txt"
 
 
+REAL_PATH="../sweep_text2ts/synth_u/orig_verbalts_orig_cap/2/samples.pt"
+FAKE_PATH="../sweep_text2ts/synth_u/orig_verbalts_orig_cap/2/samples.pt"
+#REAL_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
+#FAKE_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
+
+python calculate_fid.py \
+    --real_path ${REAL_PATH} \
+    --fake_path ${FAKE_PATH} \
+    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
+    --batch_size 128 \
+    --hidden_size 128 \
+    --num_layers 2 \
+    --num_heads 8 \
+    --latent_dim 64 \
+    --save_path "./fid_results/verbal_ts_generation_0324.txt"
+
+
+
+
 
 REAL_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
 FAKE_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
