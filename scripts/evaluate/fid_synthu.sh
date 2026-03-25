e2c6f34b4db96b0c21820ab10a96caf8f230c22f
Haochen Zhang <hz112@rice.edu>
2026-03-24 23:22:35 -0500

    0324

--- scripts/evaluate/fid_synthu.sh
+++ scripts/evaluate/fid_synthu.sh
@@ -20,39 +20,39 @@ export CUDA_VISIBLE_DEVICES=1
 #    --save_path "./fid_results/vae_embed_generation.txt"
 
 
-REAL_PATH="../sweep_text2ts/synth_u/orig_verbalts_orig_cap/2/samples.pt"
-FAKE_PATH="../sweep_text2ts/synth_u/orig_verbalts_orig_cap/2/samples.pt"
+#REAL_PATH="../sweep_text2ts/synth_u/orig_verbalts_orig_cap/2/samples.pt"
+#FAKE_PATH="../sweep_text2ts/synth_u/orig_verbalts_orig_cap/2/samples.pt"
+##REAL_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
+##FAKE_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
+#
+#python calculate_fid.py \
+#    --real_path ${REAL_PATH} \
+#    --fake_path ${FAKE_PATH} \
+#    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
+#    --batch_size 128 \
+#    --hidden_size 128 \
+#    --num_layers 2 \
+#    --num_heads 8 \
+#    --latent_dim 64 \
+#    --save_path "./fid_results/verbal_ts_generation_0324.txt"
+#
+#
+#
+#
+#
 #REAL_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
 #FAKE_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
-
-python calculate_fid.py \
-    --real_path ${REAL_PATH} \
-    --fake_path ${FAKE_PATH} \
-    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
-    --batch_size 128 \
-    --hidden_size 128 \
-    --num_layers 2 \
-    --num_heads 8 \
-    --latent_dim 64 \
-    --save_path "./fid_results/verbal_ts_generation_0324.txt"
-
-
-
-
-
-REAL_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
-FAKE_PATH="/playpen-shared/haochenz/sweep_text2ts/synth_u/orig_verbalts_orig_cap_precompute_embed-lr1e-3_bs512_multistep/0/samples.pt"
-
-python calculate_fid.py \
-    --real_path ${REAL_PATH} \
-    --fake_path ${FAKE_PATH} \
-    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
-    --batch_size 128 \
-    --hidden_size 128 \
-    --num_layers 2 \
-    --num_heads 8 \
-    --latent_dim 64 \
-    --save_path "./fid_results/verbal_ts_generation_0324.txt"
+#
+#python calculate_fid.py \
+#    --real_path ${REAL_PATH} \
+#    --fake_path ${FAKE_PATH} \
+#    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
+#    --batch_size 128 \
+#    --hidden_size 128 \
+#    --num_layers 2 \
+#    --num_heads 8 \
+#    --latent_dim 64 \
+#    --save_path "./fid_results/verbal_ts_generation_0324.txt"
 
 
 
@@ -127,8 +127,8 @@ python calculate_fid.py \
 #    --save_path "./fid_results/uncond_generation.txt"
 
 
-REAL_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u/text2ts_msmdiffmv/0/verbalts_caps_samples.pt"
-FAKE_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u/text2ts_msmdiffmv/0/verbalts_caps_samples.pt"
+REAL_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u/text2ts_msmdiffmv/2/verbalts_caps_samples.pt"
+FAKE_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u/text2ts_msmdiffmv/2/verbalts_caps_samples.pt"
 python calculate_fid.py \
     --real_path ${REAL_PATH} \
     --fake_path ${FAKE_PATH} \
@@ -141,18 +141,18 @@ python calculate_fid.py \
     --save_path "./fid_results/verbalts_generation.txt"
 
 
-REAL_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/uncond_synth_u/text2ts_msmdiffmv/0/samples.pt"
-FAKE_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/uncond_synth_u/text2ts_msmdiffmv/0/samples.pt"
-python calculate_fid.py \
-    --real_path ${REAL_PATH} \
-    --fake_path ${FAKE_PATH} \
-    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
-    --batch_size 128 \
-    --hidden_size 128 \
-    --num_layers 2 \
-    --num_heads 8 \
-    --latent_dim 64 \
-    --save_path "./fid_results/uncond_generation.txt"
+#REAL_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/uncond_synth_u/text2ts_msmdiffmv/0/samples.pt"
+#FAKE_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/uncond_synth_u/text2ts_msmdiffmv/0/samples.pt"
+#python calculate_fid.py \
+#    --real_path ${REAL_PATH} \
+#    --fake_path ${FAKE_PATH} \
+#    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
+#    --batch_size 128 \
+#    --hidden_size 128 \
+#    --num_layers 2 \
+#    --num_heads 8 \
+#    --latent_dim 64 \
+#    --save_path "./fid_results/uncond_generation.txt"
 
 
 #REAL_PATH="/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save/synth_u_my_caps/text2ts_msmdiffmv/0/real_text_samples.pt"
