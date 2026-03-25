82c32d82bfb204813c6795ed2e5f75f169e6a8e7
Haochen Zhang <hz112@rice.edu>
2026-03-25 01:25:38 -0500

    0325

--- data/data.py
+++ data/data.py
@@ -383,9 +383,9 @@ class QwenV3Split(Dataset):
 
 
         self.vae_embed = None
-        # if vae_embed_path != "none":
-        #     self.vae_embed = np.load(f"{vae_embed_path}/{split}_vae.npy", allow_pickle=True)
-        #     self.vae_embed = torch.from_numpy(self.vae_embed)
+        if vae_embed_path != "none":
+            self.vae_embed = np.load(f"{vae_embed_path}/{split}_vae.npy", allow_pickle=True)
+            self.vae_embed = torch.from_numpy(self.vae_embed)
 
 
         self.caps = None
