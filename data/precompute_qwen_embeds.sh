31628d7aea39944626dd6cea06347036316f52f6
Haochen Zhang <hz112@rice.edu>
2026-03-25 21:14:20 -0500

    0325

--- data/precompute_qwen_embeds.sh
+++ data/precompute_qwen_embeds.sh
@@ -9,8 +9,8 @@ export HF_HOME=/playpen/haochenz/hf_cache
 #CAPS_PATH="/playpen-shared/haochenz/LitsDatasets/128_len_caps_one_per_channel_0309/synth_u"
 #SAVE_ROOT="/playpen-shared/haochenz/LitsDatasets/128_len_caps_one_per_channel_0309/synth_u"
 
-CAPS_PATH="/playpen-shared/haochenz/LitsDatasets/128_len_caps_one_per_image_0324/synth_u"
-SAVE_ROOT="/playpen-shared/haochenz/LitsDatasets/128_len_caps_one_per_image_0324/synth_u"
+CAPS_PATH="/playpen-shared/haochenz/LitsDatasets/128_len_ts/synth_u"
+SAVE_ROOT="/playpen-shared/haochenz/LitsDatasets/128_len_ts/synth_u"
 
 SCRIPT_PATH="precompute_qwen_embeds.py"
 
@@ -24,19 +24,19 @@ echo "Start precomputing Qwen embeddings..."
 # =========================
 # TRAIN
 # =========================
-#python $SCRIPT_PATH \
-#    --caps_path $CAPS_PATH \
-#    --save_path $SAVE_ROOT/train_embeds_qwen_seq.pt \
-#    --split train \
-#    --batch_size $BATCH_SIZE \
-#    --device $DEVICE
+python $SCRIPT_PATH \
+    --caps_path $CAPS_PATH \
+    --save_path $SAVE_ROOT/train_embeds_caps_0324.pt \
+    --split train \
+    --batch_size $BATCH_SIZE \
+    --device $DEVICE
 
 # =========================
 # VAL
 # =========================
 python $SCRIPT_PATH \
     --caps_path $CAPS_PATH \
-    --save_path $SAVE_ROOT/valid_embeds_qwen_seq.pt \
+    --save_path $SAVE_ROOT/valid_embeds_caps_0324.pt \
     --split valid \
     --batch_size $BATCH_SIZE \
     --device $DEVICE
@@ -44,11 +44,11 @@ python $SCRIPT_PATH \
 # =========================
 # TEST
 # =========================
-#python $SCRIPT_PATH \
-#    --caps_path $CAPS_PATH \
-#    --save_path $SAVE_ROOT/test_embeds_qwen_seq.pt \
-#    --split test \
-#    --batch_size $BATCH_SIZE \
-#    --device $DEVICE
-#
+python $SCRIPT_PATH \
+    --caps_path $CAPS_PATH \
+    --save_path $SAVE_ROOT/test_embeds_caps_0324.pt \
+    --split test \
+    --batch_size $BATCH_SIZE \
+    --device $DEVICE
+
 #echo "Done!"
