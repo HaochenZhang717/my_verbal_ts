3673c9a5ee7e48667cacf40cc537bfd8d9382dd0
Haochen Zhang <hz112@rice.edu>
2026-04-20 23:30:21 +0800

    0420

--- scripts/evaluate/fid_synthu.sh
+++ scripts/evaluate/fid_synthu.sh
@@ -11,7 +11,7 @@ FAKE_PATH="/mnt/unites4/playpen/haochenz/VerbalTS_reimplement/verbalts_orig_save
 python calculate_fid.py \
     --real_path ${REAL_PATH} \
     --fake_path ${FAKE_PATH} \
-    --ckpt_path "./fid_vae_ckpts/vae_synth_u/best.pt" \
+    --ckpt_path "../fid_vae_ckpts/vae_synth_u/best.pt" \
     --batch_size 128 \
     --hidden_size 128 \
     --num_layers 2 \
