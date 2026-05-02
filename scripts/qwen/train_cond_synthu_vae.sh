c45bf873ae1c47bd5cfd00378dfc9d6a270ecc9d
Haochen Zhang <hz112@rice.edu>
2026-05-02 10:57:03 +0800

    0502

--- scripts/qwen/train_cond_synthu_vae.sh
+++ scripts/qwen/train_cond_synthu_vae.sh
@@ -18,12 +18,12 @@ do
   do
     echo "Running lr=$LR bs=$BS"
 
-    export WANDB_NAME="qwen_v1_caps_v2_synth_u_cosine-lr${LR}_bs${BS}-L${LAYERS}C${CHANNELS}H${NHEADS}D${DIFFUSION_EMBEDDING_DIM}-dropout0.1"
+    export WANDB_NAME="qwen_v1_vae_synth_u_cosine-lr${LR}_bs${BS}-L${LAYERS}C${CHANNELS}H${NHEADS}D${DIFFUSION_EMBEDDING_DIM}-dropout0.1"
 
     CUDA_VISIBLE_DEVICES=0 python run_qwen.py \
         --cond_modal vae \
         --training_stage finetune \
-        --save_folder ./sweep/synth_u_qwen_v1_caps_v2/lr_${LR}_bs_${BS}-L${LAYERS}C${CHANNELS}H${NHEADS}D${DIFFUSION_EMBEDDING_DIM}-dropout0.1 \
+        --save_folder ./sweep/synth_u_qwen_vae/lr_${LR}_bs_${BS}-L${LAYERS}C${CHANNELS}H${NHEADS}D${DIFFUSION_EMBEDDING_DIM}-dropout0.1 \
         --model_diff_config_path configs/synth_u_qwen/diff/model_text2ts_dep.yaml \
         --model_cond_config_path configs/synth_u_qwen/cond/text_msmdiffmv.yaml \
         --train_config_path configs/synth_u_qwen/train.yaml \
